1468fa86cca4ec1780b5aceba217954c7fb984af
Wilson, Nolan <Nolan.Wilson@nrel.gov>
2022-10-03 16:02:52 -0600

    Update __init__.py

--- build/lib/m2p/__init__.py
+++ build/lib/m2p/__init__.py
@@ -1,3 +1,7 @@
 from .monomers import Monomer
 from .polyestimator import PolyEstimator
 from .polymaker import PolyMaker
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
